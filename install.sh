34cbcae0b8effb63b1ec90473ae9f7c4f1b744a7
Shiva Reddy <kotishiva@gmail.com>
2016-05-03 17:54:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
 ##vagrant init hashicorp/precise32
-sudo apt-get update
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository -y ppa:ubuntugis/ppa
-sudo apt-get update
-sudo apt-get install -y python-dev
-
 sudo apt-get install -y libgdal1-dev
 sudo apt-get install -y libgdal-dev
 sudo apt-get install -y g++
 sudo apt-get install -y python-gdal
 sudo apt-get install -y python-pip
 
-sudo apt-get update
 
 sudo pip install shapely
 sudo pip install six
